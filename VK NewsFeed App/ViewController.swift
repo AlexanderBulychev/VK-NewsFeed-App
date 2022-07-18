b5569ce84b342feb27782b3a61b1a858ab5e48e3
Alexander Bulychev <asbul@yandex.ru>
2022-07-18 20:58:19 +0300

    Initial Commit

---
+++ VK NewsFeed App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK NewsFeed App
+//
+//  Created by asbul on 18.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
