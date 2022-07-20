9306064b58fbd422fac290e2b9bfdf1bab35c4d3
Alexander Bulychev <asbul@yandex.ru>
2022-07-20 21:44:00 +0300

    3 VK SDK and User Authorization: AuthVC is made initial VC, tested authorization buttonAction

--- VK NewsFeed App/Auth/AuthViewController.swift
+++ VK NewsFeed App/Auth/AuthViewController.swift
@@ -1,29 +1,12 @@
-//
-//  AuthViewController.swift
-//  VK NewsFeed App
-//
-//  Created by asbul on 20.07.2022.
-//
-
 import UIKit
 
 class AuthViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func signInTouch() {
+        print("test touch button")
     }
-    */
-
 }
