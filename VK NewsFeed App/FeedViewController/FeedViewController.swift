798fdf17f39b8b570a2928818d36d82b6fe6c176
Alexander Bulychev <asbul@yandex.ru>
2022-07-27 15:00:10 +0300

    5 Implementing Parsing Logic: create struct Feed Response for future JSON Serialization

--- VK NewsFeed App/FeedViewController/FeedViewController.swift
+++ VK NewsFeed App/FeedViewController/FeedViewController.swift
@@ -2,13 +2,19 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
-let networkService = NetworkService()
+    let networkService: Networking = NetworkService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
-        networkService.request(path: API.newsFeed, params: ["filters": "post,photo"]) { (data, error) in
-            print(error ?? "No error description")
+        let params = ["filters": "post,photo"]
+        networkService.request(path: API.newsFeed, params: params) { (data, error) in
+            if let error = error {
+                print("Error received requesting data: \(error.localizedDescription)")
+            }
+            guard let data = data else { return }
+            let json = try? JSONSerialization.jsonObject(with: data, options: [])
+            print("json: \(json)")
         }
     }
 }
